65811922c7f6510e22ff0132042177e7f01c14d2
Leonardo Uno <leouno99@gmail.com>
2023-08-23 18:32:05 -0300

    Componente register-user criado

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 
 @NgModule({
   declarations: [
   
-    LoginComponent
+    LoginComponent,
+       RegisterUserComponent
   ],
   imports: [
     CommonModule,
