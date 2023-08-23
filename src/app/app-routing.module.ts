3aa3ea9d3d500b83ed9c7b08418407a9caefad77
Leonardo Uno <leouno99@gmail.com>
2023-08-22 22:29:05 -0300

    Componente task-list criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/task-list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'task-list',
+    loadChildren: () => import('./task-list/task-list.module').then(m => m.TaskListModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
