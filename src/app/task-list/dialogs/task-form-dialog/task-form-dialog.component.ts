9e91953e718b93e08dca6f545ea46014f408aa6b
Leonardo Uno <leouno99@gmail.com>
2023-08-22 23:36:09 -0300

    Componente TaskFormDialog criado

---
+++ src/app/task-list/dialogs/task-form-dialog/task-form-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form-dialog',
+  templateUrl: './task-form-dialog.component.html',
+  styleUrls: ['./task-form-dialog.component.scss']
+})
+export class TaskFormDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
