bbdcb66d176785c9567140f9d07d295440aec986
Leonardo Uno <leouno99@gmail.com>
2023-08-22 23:17:12 -0300

    Componente task-list criado

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-task-list',
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
+
+  taskList: Task[] = [
+    {
+      name: "Criar um aplicativo de gerenciamento de tarefas",
+      description: "Este portal precisará visualizar, criar, editar e apagar tarefas, assim como ter uma tela de login para acesso e criação de usuários",
+      done: false
+    },
+    {
+      name: "Criar um aplicativo de gerenciamento de tarefas",
+      description: "Este portal precisará visualizar, criar, editar e apagar tarefas, assim como ter uma tela de login para acesso e criação de usuários",
+      done: false
+    },
+    {
+      name: "Criar um aplicativo de gerenciamento de tarefas",
+      description: "Este portal precisará visualizar, criar, editar e apagar tarefas, assim como ter uma tela de login para acesso e criação de usuários",
+      done: false
+    }
+  ]
 
   constructor() { }
 
--- src/app/task-list/task-list.module.ts
+++ src/app/task-list/task-list.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { TaskListRoutingModule } from './task-list-routing.module';
 import { TaskListComponent } from './task-list.component';
+import { MatCardModule } from '@angular/material/card';
+import { TaskCardComponent } from './task-card/task-card.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    TaskListComponent
+    TaskListComponent,
+    TaskCardComponent
   ],
   imports: [
     CommonModule,
-    TaskListRoutingModule
+    TaskListRoutingModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule
   ]
 })
 export class TaskListModule { }
