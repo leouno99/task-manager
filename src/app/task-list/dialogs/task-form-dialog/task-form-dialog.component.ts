790746276b7222f0f7f30e11a92a4f3253a34b4e
Leonardo Uno <leouno99@gmail.com>
2023-08-23 15:31:18 -0300

    Dialog para criação de tarefa

--- src/app/task-list/dialogs/task-form-dialog/task-form-dialog.component.ts
+++ src/app/task-list/dialogs/task-form-dialog/task-form-dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Task } from 'src/app/models/task';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form-dialog',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskFormDialogComponent implements OnInit {
 
-  constructor() { }
+  form = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    description: new FormControl(null, [Validators.required]),
+    done: new FormControl(false)
+  })
+
+  constructor(public dialogRef: MatDialogRef<TaskFormDialogComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  onCreateTask() {
+    let newTask: Task = this.form.value;
+
+    this.dialogRef.close(newTask)
   }
 
 }
--- src/app/task-list/task-list.module.ts
+++ src/app/task-list/task-list.module.ts
@@ -7,19 +7,29 @@ import { MatCardModule } from '@angular/material/card';
 import { TaskCardComponent } from './task-card/task-card.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { TaskFormDialogComponent } from './dialogs/task-form-dialog/task-form-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     TaskListComponent,
-    TaskCardComponent
+    TaskCardComponent,
+    TaskFormDialogComponent
   ],
   imports: [
     CommonModule,
     TaskListRoutingModule,
     MatCardModule,
     MatDividerModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ]
 })
 export class TaskListModule { }
