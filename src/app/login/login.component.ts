900e853a4b4cd54d3a638b09db1ad31e58b35d4c
Leonardo Uno <leouno99@gmail.com>
2023-08-23 21:17:17 -0300

    Funcionalidade de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form = new FormGroup({
+    user: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required])
+  });
+
+  hidePassword = true;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.form.value.user === "leo" && this.form.value.password ==="123") {
+      this.router.navigate(["/task-list"])
+    }
+    else {
+      window.alert("Credenciais inválidas");
+    }
   }
 
 }
