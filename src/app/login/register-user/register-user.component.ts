20de6c97e23172c07f28c8950d16c3bf67a63e73
Leonardo Uno <leouno99@gmail.com>
2023-08-24 00:15:41 -0300

    Funcionalidade de registro de usuário

--- src/app/login/register-user/register-user.component.ts
+++ src/app/login/register-user/register-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-user',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor() { }
+  form = new FormGroup({
+    user: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required]),
+    confirmPassword: new FormControl("", [Validators.required])
+  });
+
+  hidePassword = true;
+  hideConfirmPassword = true;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.form.value.password !== this.form.value.confirmPassword) {
+      window.alert("Valores diferentes nos campos de Senha e Confirmar senha");
+    }
+    else {
+      this.router.navigate(["/login"]);
+    }
   }
 
 }
